fc4f75bc94517da6a809572ceee117906a67512f
JewelNight <jathefo@126.com>
2018-06-20 11:30:09 +0800

    Add cygwin environment and suppoet some commands like gcc, make

---
+++ Install.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set INSTALL_DIR=%~dp0\src-install
+
+:: Install cygwin command packages
+:: %INSTALL_DIR%\setup-x86_64.exe -q -n -s http://cygwin.mirror.constant.com/ -R D:\ok-cmder\vendor\git-for-windows\cygwin
+
+:: Install gcc compiler
+%INSTALL_DIR%\setup-x86_64.exe -q -n -s http://cygwin.mirror.constant.com/ --root %~dp0\vendor\cygwin -l %INSTALL_DIR%\tmp -P gcc-core -P gcc-g++ -P make -p gdb -P binutils
+rd /s /q %INSTALL_DIR%\tmp
+
+echo "Success to install..."
+
+pause
