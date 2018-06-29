9d3b4728db768d943e8dadd6303c297933bdc544
JewelNight <jathefo@126.com>
2018-06-29 11:12:07 +0800

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -6,7 +6,10 @@ set INSTALL_DIR=%~dp0\src-install
 :: %INSTALL_DIR%\setup-x86_64.exe -q -n -s http://cygwin.mirror.constant.com/ -R D:\ok-cmder\vendor\git-for-windows\cygwin
 
 :: Install gcc compiler
-%INSTALL_DIR%\setup-x86_64.exe -q -n -W -s http://cygwin.mirror.constant.com/ --root %~dp0\vendor\cygwin -l %INSTALL_DIR%\tmp -P gcc-core -P gcc-g++ -P make -p gdb -P binutils
+%INSTALL_DIR%\setup-x86_64.exe -q -n -W -s http://cygwin.mirror.constant.com/ --root %~dp0\vendor\cygwin -l %INSTALL_DIR%\tmp ^
+-P gcc-core -P gcc-g++ -P make -p gdb -P binutils ^
+-P cmake
+
 rd /s /q %INSTALL_DIR%\tmp
 
 echo "Success to install..."
